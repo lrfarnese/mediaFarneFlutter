e8dd114c279e27613b3f1b26b2fcb4636f7a17c5
Lucas Farnese <lrfarnese@gmail.com>
2026-04-29 23:27:14 -0300

    Adicionando um botão logout que havia esquecido, 23:28

--- lib/view/screens/main/widgets/app_bar_widget.dart
+++ lib/view/screens/main/widgets/app_bar_widget.dart
@@ -1,20 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:mediafarnetcc/view/core/theme/app_colors.dart';
+import 'package:mediafarnetcc/view/screens/splash/splash1.dart';
 
 class AppBarWidget extends StatelessWidget implements PreferredSizeWidget{
 
   const AppBarWidget({super.key});
+
+  void _logout(BuildContext context) {
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => const Splash1()),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
       backgroundColor: AppColors.azulPrincipal,
       title: Image.asset(
-          'assets/images/logo.png',
-          width: 200,
-          height: 180,
+        'assets/images/logo.png',
+        height: kToolbarHeight * 0.8,
+        fit: BoxFit.contain,
       ),
       centerTitle: true,
+      actions: [
+        IconButton(
+          icon: const Icon(Icons.logout, color: AppColors.white),
+          tooltip: 'Sair',
+          onPressed: () => _logout(context),
+        ),
+      ],
     );
   }
 
